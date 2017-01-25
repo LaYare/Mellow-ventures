688d15e121b560ff28eb84b27cb87f9c969b3fad
YareArista <yarearista@gmail.com>
2017-01-25 16:52:46 -0600

    linea del tiempo terminada

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navegación
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['home1', 'portafolio1', 'equipo1', 'contacto1'],
+        navigation: true,
+        navigationPosition: 'left',
+        navigationTooltips: ['NOSOTROS', 'PORTAFOLIO', 'EQUIPO', 'CONTACTO'],
+        showActiveTooltip: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Desplazamiento
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accesibilidad
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Diseno
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['', ''],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+
+        //Selectores personalizados
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //Eventos
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
